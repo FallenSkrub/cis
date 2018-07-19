293ab1ddbac2d1734360b69f674cff5671c0dfb5
FallenSkrub <sam02kl@gmail.com>
2018-07-19 17:38:30 +0000

    Project 5 of Assignment 16, continuing on?

---
+++ Assignment_16/16Prog5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int adding(int number)
+{
+    if (number == 10){
+        return number;
+    }
+    else {
+        cout << number + 1 << endl;
+        adding(number+1);
+    }
+}
+int main() {
+    int t = 0;
+    adding(t);
+
+    return 0;
+}
